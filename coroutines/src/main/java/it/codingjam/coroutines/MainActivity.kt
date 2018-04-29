5492528e4c543054568f4eb87851265fdf623648
Fabio Collini <fabio.collini@gmail.com>
2018-04-29 21:22:22 +0200

    Renamed packages

--- coroutines/src/main/java/it/codingjam/coroutines/MainActivity.kt
+++ coroutines/src/main/java/it/codingjam/coroutines/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.droidcon.rxjava
+package it.codingjam.coroutines
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
--- rx/src/main/java/it/codingjam/rxjava/MainActivity.kt
+++ rx/src/main/java/it/codingjam/rxjava/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.droidcon.coroutines
+package it.codingjam.rxjava
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
