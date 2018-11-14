132020b1f09d65802700199b4734342494dded68
fabioCollini <fabio.collini@gmail.com>
2018-11-13 22:17:06 -0500

    Porting to AndroidX

--- coroutines/src/main/java/it/codingjam/common/arch/ViewModelDelegate.kt
+++ coroutines/src/main/java/it/codingjam/common/arch/ViewModelDelegate.kt
@@ -1,9 +1,9 @@
 package it.codingjam.common.arch
 
-import android.arch.lifecycle.ViewModel
-import android.arch.lifecycle.ViewModelProvider
-import android.arch.lifecycle.ViewModelProviders
-import android.support.v4.app.FragmentActivity
+import androidx.fragment.app.FragmentActivity
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProviders
 
 inline fun <reified VM : ViewModel> viewModel(activity: FragmentActivity, crossinline factory: () -> VM): Lazy<VM> {
     return lazy {
