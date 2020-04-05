38bfa03e4a73157468e71bed936c5118daa0e50f
fabioCollini <fabio.collini@gmail.com>
2020-04-05 14:46:33 +0200

    New example

---
+++ coroutines/src/main/java/it/codingjam/coroutines/ViewModel1_2.kt
@@ -0,0 +1,32 @@
+package it.codingjam.coroutines
+
+import androidx.lifecycle.MutableLiveData
+import it.codingjam.common.UserStats
+import kotlinx.coroutines.Dispatchers.IO
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
+class ViewModel1_2(private val service: StackOverflowServiceCoroutines) : ViewModel() {
+
+    val state = MutableLiveData<String>()
+
+    fun load() {
+        viewModelScope.launch {
+            try {
+                val usersWithBadges = withContext(IO) {
+                    val users = service.getTopUsers()
+                    val firstUser = users.first()
+                    val badges = service.getBadges(firstUser.id)
+                    UserStats(firstUser, badges)
+                }
+                updateUi(usersWithBadges)
+            } catch (e: Exception) {
+                updateUi(e)
+            }
+        }
+    }
+
+    private fun updateUi(s: Any) {
+        state.value = s.toString()
+    }
+}
