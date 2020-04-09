c5cb1cf60f5aec37a2a6313d5107d6099fdc47df
fabioCollini <fabio.collini@gmail.com>
2020-04-09 08:55:07 +0200

    Add default param

--- demo/src/main/java/it/androiddevs/demo/CoroutinesRepository.kt
+++ demo/src/main/java/it/androiddevs/demo/CoroutinesRepository.kt
@@ -1,9 +1,10 @@
 package it.androiddevs.demo
 
 import it.codingjam.common.StackOverflowServiceCoroutines
+import it.codingjam.common.utils.ServiceFactory
 
 class CoroutinesRepository(
-        private val service: StackOverflowServiceCoroutines
+        private val service: StackOverflowServiceCoroutines = ServiceFactory.coroutines
 ) {
     suspend fun loadData() = service.getTopUsers()
 }
--- demo/src/main/java/it/androiddevs/demo/CoroutinesViewModel.kt
+++ demo/src/main/java/it/androiddevs/demo/CoroutinesViewModel.kt
@@ -3,12 +3,11 @@ package it.androiddevs.demo
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import it.codingjam.common.utils.ServiceFactory
 import kotlinx.coroutines.launch
 
 class CoroutinesViewModel : ViewModel() {
 
-    private val repository = CoroutinesRepository(ServiceFactory.coroutines)
+    private val repository = CoroutinesRepository()
 
     val state = MutableLiveData<Any>()
 
