018f0cb4929e2cf3212ae8028253ceebeebb1bcc
Fabio Collini <fabio.collini@gmail.com>
2020-04-10 18:46:35 +0200

    Move exponential backoff

---
+++ common/src/main/java/it/codingjam/common/utils/ExponentialBackoff.kt
@@ -0,0 +1,25 @@
+package it.codingjam.common.utils
+
+import io.reactivex.Flowable
+import org.reactivestreams.Publisher
+import java.util.*
+import java.util.concurrent.TimeUnit
+
+class ExponentialBackoff(private val maxRetries: Int) :
+        Function1<Flowable<out Throwable>, Publisher<*>> {
+
+    private var retryCount = 0
+    private var currentDelay = 100L + Random().nextInt(100)
+
+    override fun invoke(attempts: Flowable<out Throwable>): Publisher<*> {
+        return attempts
+                .flatMap { throwable ->
+                    if (++retryCount < maxRetries)
+                        Flowable.timer(currentDelay, TimeUnit.MILLISECONDS).also {
+                            currentDelay *= 2
+                        }
+                    else
+                        Flowable.error(throwable)
+                }
+    }
+}
--- rx/src/main/java/it/codingjam/rxjava/ViewModel6.kt
+++ rx/src/main/java/it/codingjam/rxjava/ViewModel6.kt
@@ -2,16 +2,13 @@ package it.codingjam.rxjava
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import io.reactivex.Flowable
 import io.reactivex.android.schedulers.AndroidSchedulers.mainThread
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.rxkotlin.plusAssign
 import io.reactivex.schedulers.Schedulers.io
-import it.codingjam.common.utils.ServiceFactory
 import it.codingjam.common.StackOverflowServiceRx
-import org.reactivestreams.Publisher
-import java.util.*
-import java.util.concurrent.TimeUnit
+import it.codingjam.common.utils.ExponentialBackoff
+import it.codingjam.common.utils.ServiceFactory
 import java.util.concurrent.TimeUnit.SECONDS
 
 class ViewModel6 : ViewModel() {
@@ -40,24 +37,5 @@ class ViewModel6 : ViewModel() {
 
     override fun onCleared() {
         disposable.clear()
-    }
-}
-
-class ExponentialBackoff(private val maxRetries: Int) :
-        Function1<Flowable<out Throwable>, Publisher<*>> {
-
-    private var retryCount = 0
-    private var currentDelay = 100L + Random().nextInt(100)
-
-    override fun invoke(attempts: Flowable<out Throwable>): Publisher<*> {
-        return attempts
-                .flatMap { throwable ->
-                    if (++retryCount < maxRetries)
-                        Flowable.timer(currentDelay, TimeUnit.MILLISECONDS).also {
-                            currentDelay *= 2
-                        }
-                    else
-                        Flowable.error(throwable)
-                }
     }
 }
