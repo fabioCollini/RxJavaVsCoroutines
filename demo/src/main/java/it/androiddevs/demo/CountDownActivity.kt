4509276c15662a230d0ca274f002c46cd95148ff
Fabio Collini <fabio.collini@gmail.com>
2020-04-13 12:06:58 +0200

    Restore initial example

--- demo/src/main/java/it/androiddevs/demo/CountDownActivity.kt
+++ demo/src/main/java/it/androiddevs/demo/CountDownActivity.kt
@@ -2,10 +2,7 @@ package it.androiddevs.demo
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.coroutineScope
 import kotlinx.android.synthetic.main.activity_countdown.*
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
 
 class CountDownActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,21 +10,12 @@ class CountDownActivity : AppCompatActivity() {
 
         setContentView(R.layout.activity_countdown)
 
-        lifecycle.coroutineScope.launch {
-            text_view.text = "3"
-            delay(1000)
-            text_view.text = "2"
-            delay(1000)
-            text_view.text = "1"
-            delay(1000)
-            text_view.text = "Go!"
-        }
-//        text_view.text = "3"
-//        Thread.sleep(1000)
-//        text_view.text = "2"
-//        Thread.sleep(1000)
-//        text_view.text = "1"
-//        Thread.sleep(1000)
-//        text_view.text = "Go!"
+        text_view.text = "3"
+        Thread.sleep(1000)
+        text_view.text = "2"
+        Thread.sleep(1000)
+        text_view.text = "1"
+        Thread.sleep(1000)
+        text_view.text = "Go!"
     }
 }
