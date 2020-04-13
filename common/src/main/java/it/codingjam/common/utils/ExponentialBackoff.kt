fde2c93398dc8c9e89dfc4c3d227791610d6d4f3
Fabio Collini <fabio.collini@gmail.com>
2020-04-13 15:03:14 +0200

    Use kotlin Random

--- common/src/main/java/it/codingjam/common/utils/ExponentialBackoff.kt
+++ common/src/main/java/it/codingjam/common/utils/ExponentialBackoff.kt
@@ -2,14 +2,14 @@ package it.codingjam.common.utils
 
 import io.reactivex.Flowable
 import org.reactivestreams.Publisher
-import java.util.*
 import java.util.concurrent.TimeUnit
+import kotlin.random.Random
 
 class ExponentialBackoff(private val maxRetries: Int) :
         Function1<Flowable<out Throwable>, Publisher<*>> {
 
     private var retryCount = 0
-    private var currentDelay = 100L + Random().nextInt(100)
+    private var currentDelay = 100L + Random.nextInt(100)
 
     override fun invoke(attempts: Flowable<out Throwable>): Publisher<*> {
         return attempts
